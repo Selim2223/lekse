90f8464e69077a0ad14e839c63f50f0b04d65785
Zelimkhan <delfinbel@gmail.com>
2023-01-25 09:43:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let myAge = 36;
+let myNewAge = 18;
+ console.log(myAge);
+
+ myAge = myNewAge;
+ console.log(myAge);
+
+ let trueOrFalse = 58 > 18;
+ console.log(trueOrFalse);
+
+
+willYouMarry = false;
+if (willYouMarry) {
+   console.log(':)');
+} else {
+   console.log(':(');
+}
+
+willYouMarry = true;
+if (willYouMarry) {
+   console.log(':)');
+} else {
+   console.log(':(');
+}
+
+let userAge = 20;
+let userHeight = 1.83;
+console.log(userAge);
+console.log(typeof userAge);
+console.log(userHeight);
+console.log(typeof userHeight);
